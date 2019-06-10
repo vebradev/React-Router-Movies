5a5e2ace6af6a6e9cfa926070c6c6552ba8510b7
vebradev <justinas@vebra.dev>
2019-06-10 16:39:59 +0300

    Convert Link to NavLink in SavedList component.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -11,9 +11,12 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <Link to={`/movies/${movie.id}`}>
+          <NavLink to={`/movies/${movie.id}`} activeStyle={{
+            fontWeight: "bold",
+            color: "#212121",
+          }}>
             <span className="saved-movie">{movie.title}</span>
-          </Link>
+          </NavLink>
         ))}
         <Link to="/"><div className="home-button">Home</div></Link>
       </div>
